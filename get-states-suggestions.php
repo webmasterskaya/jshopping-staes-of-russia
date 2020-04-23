88589760d70e27decb1696d0d20afd9b44aead69
Artem Vasilev <kern.usr@gmail.com>
2020-04-23 21:30:12 +0300

    Оптимизировал генератор Исправил ошибку в запросе

--- get-states-suggestions.php
+++ get-states-suggestions.php
@@ -3,106 +3,92 @@
 $token = '__TOKEN__';
 
 $input = [
-	['176', '1', '1', 'Adygeya Republic', 'Adygeya Republic', 'Республика Адыгея'],
-	['176', '1', '2', 'Bashkortostan Republic', 'Bashkortostan Republic', 'Республика Башкортостан'],
-	['176', '1', '3', 'Buryatiya Republic', 'Buryatiya Republic', 'Республика Бурятия'],
-	['176', '1', '4', 'Altai', 'Altai', 'Республика Алтай'],
-	['176', '1', '5', 'Dagestan', 'Dagestan', 'Республика Дагестан'],
-	['176', '1', '6', 'Ingushetiya Republic', 'Ingushetiya Republic', 'Республика Ингушетия'],
-	[
-		'176',
-		'1',
-		'7',
-		'Kabardino-Balkariya Republic',
-		'Kabardino-Balkariya Republic',
-		'Республика Кабардино-Балкарская'
-	],
-	['176', '1', '8', 'Kalmykiya Republic', 'Kalmykiya Republic', 'Республика Калмыкия'],
-	[
-		'176',
-		'1',
-		'9',
-		'Karachayevo-Cherkesiya Republic',
-		'Karachayevo-Cherkesiya Republic',
-		'Республика Карачаево-Черкесская'
-	],
-	['176', '1', '10', 'Karelia', 'Karelia', 'Республика Карелия'],
-	['176', '1', '11', 'Komi', 'Komi', 'Республика Коми'],
-	['176', '1', '12', 'Mariy-El Republic', 'Mariy-El Republic', 'Республика Марий Эл'],
-	['176', '1', '13', 'Mordoviya Republic', 'Mordoviya Republic', 'Республика Мордовия'],
-	['176', '1', '14', 'Sakha', 'Sakha', 'Республика Саха /Якутия/'],
-	['176', '1', '15', 'North Ossetia', 'North Ossetia', 'Республика Северная Осетия - Алания'],
-	['176', '1', '16', 'Tatarstan Republic', 'Tatarstan Republic', 'Республика Татарстан'],
-	['176', '1', '17', 'Republic of Tyva', 'Republic of Tyva', 'Республика Тыва'],
-	['176', '1', '18', 'Udmurtiya Republic', 'Udmurtiya Republic', 'Республика Удмуртская'],
-	['176', '1', '19', 'Khakasiya Republic', 'Khakasiya Republic', 'Республика Хакасия'],
-	['176', '1', '20', 'Chechnya', 'Chechnya', 'Республика Чеченская'],
-	['176', '1', '21', 'Chuvashia', 'Chuvashia', 'Чувашская Республика - Чувашия'],
-	['176', '1', '22', 'Altai Krai', 'Altai Krai', 'Алтайский край'],
-	['176', '1', '23', 'Krasnodarskiy', 'Krasnodarskiy', 'Краснодарский край'],
-	['176', '1', '24', 'Krasnoyarskiy', 'Krasnoyarskiy', 'Красноярский край'],
-	['176', '1', '25', 'Primorskiy [Maritime] Kray', 'Primorskiy [Maritime] Kray', 'Приморский край'],
-	['176', '1', '26', 'Stavropol Kray', 'Stavropol Kray', 'Ставропольский край'],
-	['176', '1', '27', 'Khabarovsk', 'Khabarovsk', 'Хабаровский край'],
-	['176', '1', '28', 'Amur Oblast', 'Amur Oblast', 'Амурская область'],
-	['176', '1', '29', 'Arkhangelskaya', 'Arkhangelskaya', 'Архангельская область'],
-	['176', '1', '30', 'Astrakhan', 'Astrakhan', 'Астраханская область'],
-	['176', '1', '31', 'Belgorod Oblast', 'Belgorod Oblast', 'Белгородская область'],
-	['176', '1', '32', 'Bryansk Oblast', 'Bryansk Oblast', 'Брянская область'],
-	['176', '1', '33', 'Vladimir', 'Vladimir', 'Владимирская область'],
-	['176', '1', '34', 'Volgograd Oblast', 'Volgograd Oblast', 'Волгоградская область'],
-	['176', '1', '35', 'Vologda', 'Vologda', 'Вологодская область'],
-	['176', '1', '36', 'Voronezj', 'Voronezj', 'Воронежская область'],
-	['176', '1', '37', 'Ivanovo', 'Ivanovo', 'Ивановская область'],
-	['176', '1', '38', 'Irkutsk Oblast', 'Irkutsk Oblast', 'Иркутская область'],
-	['176', '1', '39', 'Kaliningrad', 'Kaliningrad', 'Калининградская область'],
-	['176', '1', '40', 'Kaluga', 'Kaluga', 'Калужская область'],
-	['176', '1', '41', 'Kamchatka', 'Kamchatka', 'Камчатский край'],
-	['176', '1', '42', 'Kemerovo Oblast', 'Kemerovo Oblast', 'Кемеровская областьасть - Кузбасс'],
-	['176', '1', '43', 'Kirov', 'Kirov', 'Кировская область'],
-	['176', '1', '44', 'Kostroma Oblast', 'Kostroma Oblast', 'Костромская область'],
-	['176', '1', '45', 'Kurgan Oblast', 'Kurgan Oblast', 'Курганская область'],
-	['176', '1', '46', 'Kursk', 'Kursk', 'Курская область'],
-	['176', '1', '47', 'Leningradskaya Oblast', 'Leningradskaya Oblast', 'Ленинградская область'],
-	['176', '1', '48', 'Lipetsk Oblast', 'Lipetsk Oblast', 'Липецкая область'],
-	['176', '1', '49', 'Magadan Oblast', 'Magadan Oblast', 'Магаданская область'],
-	['176', '1', '50', 'Moscow Oblast', 'Moscow Oblast', 'Московская область'],
-	['176', '1', '51', 'Murmansk', 'Murmansk', 'Мурманская область'],
-	['176', '1', '52', 'Nizhny Novgorod Oblast', 'Nizhny Novgorod Oblast', 'Нижегородская область'],
-	['176', '1', '53', 'Novgorod Oblast', 'Novgorod Oblast', 'Новгородская область'],
-	['176', '1', '54', 'Novosibirsk Oblast', 'Novosibirsk Oblast', 'Новосибирская область'],
-	['176', '1', '55', 'Omsk', 'Omsk', 'Омская область'],
-	['176', '1', '56', 'Orenburg Oblast', 'Orenburg Oblast', 'Оренбургская область'],
-	['176', '1', '57', 'Orel Oblast', 'Orel Oblast', 'Орловская область'],
-	['176', '1', '58', 'Penza', 'Penza', 'Пензенская область'],
-	['176', '1', '59', 'Perm', 'Perm', 'Пермский край'],
-	['176', '1', '60', 'Pskov Oblast', 'Pskov Oblast', 'Псковская область'],
-	['176', '1', '61', 'Rostov', 'Rostov', 'Ростовская область'],
-	['176', '1', '62', 'Ryazan Oblast', 'Ryazan Oblast', 'Рязанская область'],
-	['176', '1', '63', 'Samara Oblast', 'Samara Oblast', 'Самарская область'],
-	['176', '1', '64', 'Saratovskaya Oblast', 'Saratovskaya Oblast', 'Саратовская область'],
-	['176', '1', '65', 'Sakhalin Oblast', 'Sakhalin Oblast', 'Сахалинская область'],
-	['176', '1', '66', 'Sverdlovsk', 'Sverdlovsk', 'Свердловская область'],
-	['176', '1', '67', 'Smolensk', 'Smolensk', 'Смоленская область'],
-	['176', '1', '68', 'Tambov', 'Tambov', 'Тамбовская область'],
-	['176', '1', '69', 'Tver Oblast', 'Tver Oblast', 'Тверская область'],
-	['176', '1', '70', 'Tomsk Oblast', 'Tomsk Oblast', 'Томская область'],
-	['176', '1', '71', 'Tula', 'Tula', 'Тульская область'],
-	['176', '1', '72', 'Tyumen Oblast', 'Tyumen Oblast', 'Тюменская область'],
-	['176', '1', '73', 'Ulyanovsk', 'Ulyanovsk', 'Ульяновская область'],
-	['176', '1', '74', 'Chelyabinsk', 'Chelyabinsk', 'Челябинская область'],
-	['176', '1', '75', 'Transbaikal Territory', 'Transbaikal Territory', 'Забайкальский край'],
-	['176', '1', '76', 'Jaroslavl', 'Jaroslavl', 'Ярославская область'],
-	['176', '1', '77', 'Moscow', 'Moscow', ' город Москва'],
-	['176', '1', '78', 'St.-Petersburg', 'St.-Petersburg', ' город Санкт-Петербург'],
-	['176', '1', '79', 'Jewish Autonomous Oblast', 'Jewish Autonomous Oblast', 'Еврейская Аобл'],
-	['176', '1', '80', 'Nenets', 'Nenets', 'Ненецкий АО'],
-	['176', '1', '81', 'Khanty-Mansia', 'Khanty-Mansia', 'Ханты-Мансийский Автономный окру город - Югра'],
-	['176', '1', '82', 'Chukotka', 'Chukotka', 'Чукотский АО'],
-	['176', '1', '83', 'Yamalo-Nenets', 'Yamalo-Nenets', 'Ямало-Ненецкий АО'],
-	['176', '1', '84', 'Crimea', 'Crimea', 'Республика Крым'],
-	['176', '1', '85', 'Sevastopol City', 'Sevastopol City', ' город Севастополь'],
-	['176', '1', '86', 'Baikonur', 'Baikonur', ' город Байконур']
+	'Республика Адыгея',
+	'Республика Башкортостан',
+	'Республика Бурятия',
+	'Республика Алтай',
+	'Республика Дагестан',
+	'Республика Ингушетия',
+	'Республика Кабардино-Балкарская',
+	'Республика Калмыкия',
+	'Республика Карачаево-Черкесская',
+	'Республика Карелия',
+	'Республика Коми',
+	'Республика Марий Эл',
+	'Республика Мордовия',
+	'Республика Саха /Якутия/',
+	'Республика Северная Осетия - Алания',
+	'Республика Татарстан',
+	'Республика Тыва',
+	'Республика Удмуртская',
+	'Республика Хакасия',
+	'Республика Чеченская',
+	'Чувашская Республика - Чувашия',
+	'Алтайский край',
+	'Краснодарский край',
+	'Красноярский край',
+	'Приморский край',
+	'Ставропольский край',
+	'Хабаровский край',
+	'Амурская область',
+	'Архангельская область',
+	'Астраханская область',
+	'Белгородская область',
+	'Брянская область',
+	'Владимирская область',
+	'Волгоградская область',
+	'Вологодская область',
+	'Воронежская область',
+	'Ивановская область',
+	'Иркутская область',
+	'Калининградская область',
+	'Калужская область',
+	'Камчатский край',
+	'Кемеровская областьасть - Кузбасс',
+	'Кировская область',
+	'Костромская область',
+	'Курганская область',
+	'Курская область',
+	'Ленинградская область',
+	'Липецкая область',
+	'Магаданская область',
+	'Московская область',
+	'Мурманская область',
+	'Нижегородская область',
+	'Новгородская область',
+	'Новосибирская область',
+	'Омская область',
+	'Оренбургская область',
+	'Орловская область',
+	'Пензенская область',
+	'Пермский край',
+	'Псковская область',
+	'Ростовская область',
+	'Рязанская область',
+	'Самарская область',
+	'Саратовская область',
+	'Сахалинская область',
+	'Свердловская область',
+	'Смоленская область',
+	'Тамбовская область',
+	'Тверская область',
+	'Томская область',
+	'Тульская область',
+	'Тюменская область',
+	'Ульяновская область',
+	'Челябинская область',
+	'Забайкальский край',
+	'Ярославская область',
+	' город Москва',
+	' город Санкт-Петербург',
+	'Еврейская Аобл',
+	'Ненецкий АО',
+	'Ханты-Мансийский Автономный окру город - Югра',
+	'Чукотский АО',
+	'Ямало-Ненецкий АО',
+	'Республика Крым',
+	' город Севастополь',
+	' город Байконур'
 ];
 
 $output = [];
@@ -110,7 +96,7 @@ $output = [];
 $headers   = array();
 $headers[] = 'Content-Type: application/json';
 $headers[] = 'Accept: application/json';
-$headers[] = 'Authorization: Token '.$token;
+$headers[] = 'Authorization: Token ' . $token;
 
 $ch = curl_init();
 
@@ -124,29 +110,41 @@ $lang = [
 	4 => 'en',
 	5 => 'ru',
 ];
-
-foreach ($input as $row)
+$i    = 0;
+foreach ($input as $item)
 {
-	$new = $row;
+	$i++;
+	$new = [176, 1, $i, '', '', ''];
 
+	$suggestionGet = false;
 	foreach ($lang as $p => $l)
 	{
 		curl_setopt($ch, CURLOPT_POSTFIELDS,
-			"{ \"query\": \"" . trim($row[5]) . "\", \"count\": 1, \"language\": \"" . $l . "\" }");
+			"{ \"query\": \"" . trim($item) . "\", \"count\": 1, \"language\": \"" . $l . "\" }");
 
 		$result = curl_exec($ch);
 
 		if (curl_errno($ch))
 		{
 			echo 'Error:' . curl_error($ch);
+			break;
 		}
 
 		$region = @json_decode($result, true)['suggestions'][0]['data']['region_with_type'];
 
 		if (!empty($region))
 		{
-			$new[$p] = $region;
+			$suggestionGet = true;
+			$new[$p]       = $region;
 		}
+		else
+		{
+			break;
+		}
+	}
+	if (!$suggestionGet)
+	{
+		continue;
 	}
 	$output[] = $new;
 }
@@ -171,43 +169,15 @@ $create = 'create table if not exists `#__jshopping_states`
     `name_ru-RU`    varchar(255) not null,
     primary key (`state_id`)
 );';
+$delete = 'delete from `#__jshopping_states` where `country_id` = 176;';
 $insert = 'insert into `#__jshopping_states` (`country_id`, `state_publish`, `ordering`, `name_en-GB`, `name_de-DE`, `name_ru-RU`)';
-$delete = 'delete from `#__jshopping_states` where `country_id` = 176';
 
-$files = [
-	'install' => 'install.sql',
-	'update'  => 'update.sql',
-];
-
-// Create install.sql
+// Create update.sql
 ob_start();
 echo $copy;
 echo PHP_EOL;
 echo PHP_EOL;
 echo $create;
-echo PHP_EOL;
-echo PHP_EOL;
-echo $insert;
-echo PHP_EOL;
-echo 'values';
-foreach ($output as $n => $row)
-{
-	$str = " ('" . implode("', '", $row) . "'),";
-	if ($n > 0)
-	{
-		$str = '      ' . $str;
-	}
-	if (!isset($output[$n + 1]))
-	{
-		$str = rtrim($str, ',') . ";";
-	}
-	echo $str . PHP_EOL;
-}
-file_put_contents(dirname(__FILE__) . '/' . $files['install'], ob_get_clean());
-
-// Create update.sql
-ob_start();
-echo $copy;
 echo PHP_EOL;
 echo PHP_EOL;
 echo $delete;
@@ -229,4 +199,4 @@ foreach ($output as $n => $row)
 	}
 	echo $str . PHP_EOL;
 }
-file_put_contents(dirname(__FILE__) . '/' . $files['update'], ob_get_clean());
+file_put_contents(dirname(__FILE__) . '/update.sql', ob_get_clean());
